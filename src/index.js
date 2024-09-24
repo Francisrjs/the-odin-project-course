9545decb9c4bd299cab2aa09568879c916163a49
francisrjs <franrojas331@gmail.com>
2024-09-23 21:06:42 -0300

    add functions buttons in projects and projects carts view with js

--- src/index.js
+++ src/index.js
@@ -1,3 +1,4 @@
+import { ca } from "date-fns/locale";
 import "./extras.css"
 import "./main.css";
 import "./reset.css";
@@ -5,6 +6,9 @@ console.log("hola web  ss");
 const projectAdd= document.querySelector(".add-project");
 const projectForm=document.querySelector(".project-form");
 const btnSaveProject=document.querySelector(".save-form-project");
+const todoAdd= document.querySelector(".add-todo");
+const todoForm=document.querySelector(".todo-form");
+const btnSaveTodo=document.querySelector(".save-form-todo");
 function extractForm() {
     const title = document.getElementById("input-title").value;
     const date = document.getElementById("input-date").value;
@@ -21,7 +25,7 @@ function extractForm() {
     const div = document.createElement("div");
   
     const projects = document.querySelector(".projects");
-  
+    const carts= document.querySelector(".project-view");
     h2.textContent = element.title; // Use textContent for security
     h3.textContent = element.date;
     p.textContent = element.description;
@@ -32,18 +36,47 @@ function extractForm() {
   
     if (type === "project") {
       div.className = "project-cart";
+      div.id=element.title;
       projects.appendChild(div); // Append the new div to projects
     } else {
       // Handle priority differently if needed in future versions
+      div.className ="project-view-cart";
+      div.id=element.title;
+      carts.appendChild(div); // Append the new div to projects
+
     }
   }
-projectAdd.addEventListener("click",()=>{
-    projectForm.setAttribute("id", "show");
-    console.log("click");
+function formAdd(element){
+  console.log(element);
+    element.addEventListener("click",()=>{
+      if(element.id=="project"){
+      projectForm.setAttribute("id", "show");
+      }else{
+      todoForm.setAttribute("id","show");
+      }
+      console.log("click");
+  })
+  }
+function saveForm(element){
+  console.log(element.id);
+  element.addEventListener("click",()=>{
+    console.log(document.getElementById("input-title").value);
+      const formData = extractForm();
+      console.log(formData.description);
+      createElement(formData,element.id);
+      if(element.id=="project"){
+        projectForm.setAttribute("id", "hidden");
+      }else{
+        todoForm.setAttribute("id", "hidden");
+        console.log("TODO");
+      }
+      
+  })
+}
+formAdd(projectAdd);
+formAdd(todoAdd);
+saveForm(btnSaveProject);
+saveForm(btnSaveTodo);
 
-})
-btnSaveProject.addEventListener("click",()=>{
-    const formData = extractForm();
-    console.log(formData.description);
-    createElement(formData,"project");
-})
+
+
