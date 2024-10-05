7ccd64c82ec55e5d5d9f518d96ef227af96107a2
francisrjs <franrojas331@gmail.com>
2024-10-05 13:42:06 -0300

    webpack live in git

--- webpack.prod.js
+++ webpack.prod.js
@@ -1,18 +1,13 @@
-// webpack.config.js
 const path = require("path");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 
 module.exports = {
-  mode: "development",
+  mode: "production",
   entry: "./src/index.js",
   output: {
     filename: "index.js",
     path: path.resolve(__dirname, "dist"),
     clean: true,
-  },
-  devtool: "eval-source-map",
-  devServer: {
-    watchFiles: ["./src/index.html"],
   },
   plugins: [
     new HtmlWebpackPlugin({
